24a175484996e6286d13aaf9874a041e958aeeb8
Roman <romander@ukr.net>
2017-11-29 20:50:29 +0200

    lesson1

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const spritesmith = require('gulp.spritesmith');
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
+
+gulp.task('server', function() {
+	browserSync.init({
+		server: {
+			port: 9000,
+			baseDir: "build"
+		}
+	});
+});
+
+gulp.watch('build/**/*').on('change', browserSync.reload);
+
+gulp.task('template:compile', function buildHTML() {
+  return gulp.src('source/template/index.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task('styles:compile', function () {
+  return gulp.src('source/styles/global/main.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('sprite', function (cb) {
+  var spriteData = gulp.src('source/images/icons/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    imgPath: '../images/sprite.png',
+    cssName: 'sprite.scss'
+    
+  }));
+  spriteData.img.pipe(gulp.dest('build/images/'));
+  spriteData.css.pipe(gulp.dest('source/styles/global/'));
+  cb();
+});
+
+gulp.task('clean', function del(cb) {
+	return rimraf('build', cb);
+});
+
+gulp.task('copy:images', function() {
+	return gulp.src('./source/images/**/*.*')
+	.pipe(gulp.dest('build/images'));
+});
+
+gulp.task('copy:fonts', function() {
+	return gulp.src('./source/fonts/**/*.*')
+	.pipe(gulp.dest('build/fonts'));
+});
+
+gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
+
+gulp.task('watch', function() {
+	gulp.watch('source/template/**/*.pug', gulp.series('template:compile'));
+	gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+});
+
+gulp.task('default', gulp.series(
+	'clean',
+	gulp.parallel('template:compile', 'styles:compile', 'sprite', 'copy'),
+	gulp.parallel('watch', 'server')
+	));
